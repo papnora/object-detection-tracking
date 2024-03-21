47f87cb5b47e6c15f488394c14a3ba6e58f132cc
Pap Nóra <jamansardin@gmail.com>
2024-03-21 07:59:37 +0000

    Update label

--- scripts/starter_script.py
+++ scripts/starter_script.py
@@ -3,6 +3,7 @@ print(torch.__version__)
 print(torch.cuda.is_available())
 import sys
 import cv2
+import yaml
 sys.path.append('/notebooks/ObjectDetectionTracking_PN')
 from deepsort_tracking import DeepSORT 
 import os 
@@ -11,12 +12,19 @@ import pandas as pd
 from pathlib import Path
 from tqdm import tqdm
 
-csv_path = '/notebooks/ObjectDetectionTracking_PN/datas/detections/hongkong_pedestrians1.csv'
+yaml_path = '/notebooks/ObjectDetectionTracking_PN/yolov7/data/coco.yaml'
+with open(yaml_path, 'r') as yaml_file:
+    coco_data = yaml.safe_load(yaml_file)
+names = coco_data['names']
+
+#csv_path = '/notebooks/ObjectDetectionTracking_PN/datas/detections/hongkong_pedestrians.csv'
+csv_path = '/notebooks/ObjectDetectionTracking_PN/datas/detections/park_people.csv'
 df = pd.read_csv(csv_path)
 print(df)
 print("A beolvasás sikeres volt.")
 
-video_path = '/notebooks/ObjectDetectionTracking_PN/datas/videos/hongkong_pedestrians.mp4'
+video_path = '/notebooks/ObjectDetectionTracking_PN/datas/videos/park_people.mp4'
+#video_path = '/notebooks/ObjectDetectionTracking_PN/datas/videos/hongkong_pedestrians.mp4'
 tracking_video_out = Path('/notebooks/ObjectDetectionTracking_PN/datas/videos/deepsort_out.avi')
 
 
@@ -43,15 +51,21 @@ try:
         
         #[print(d) for d in pred.itertuples()]
         tracker.update(list(pred.itertuples()))
-        #print(tracker.tracks)
     
         # kirajzolás
         for track in tracker.tracks:
             bbox = track.bbox  # bbox koordinátái - top-left bottom-right / 4 elemű tuple lista
             track_id = track.track_id  
 
+            #'pred' DataFrame-ben 'label' oszlop - objektum kategória indexe
+            label_index = pred[pred['frame_id'] == frame_id]['label'].values[0]  #az első detektált objektum típusa minden frame-ben
+            label_name = names[label_index]  
+
+
+            cv2.putText(frame, f'ID: {track_id}', (int(bbox[0]), int(bbox[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, colors[track_id % len(colors)], 2)
+            cv2.putText(frame, label_name, (int(bbox[0]), int(bbox[1]+15)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, colors[track_id % len(colors)], 2)
             cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), colors[track_id % len(colors)], 2)
-            cv2.putText(frame, str(track_id), (int(bbox[0]), int(bbox[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, colors[track_id % len(colors)], 2)
+            
 
         out.write(frame)
         frame_id += 1
@@ -62,7 +76,3 @@ except KeyboardInterrupt:
 cap.release()
 out.release()
 cv2.destroyAllWindows()
-
-
-
-                          
