4eb205d5ffba03c880cb46d1906cd66bb8922000
Pap Nóra <jamansardin@gmail.com>
2024-03-27 17:36:10 +0000

    Update DeepSort with max_age n_init

--- scripts/deepsort_tracking.py
+++ scripts/deepsort_tracking.py
@@ -34,8 +34,10 @@ class Track(BaseTrack):
 
 class DeepSORT(BaseTracker):
     def __init__(self):
-        max_cosine_distance = 0.4
-        nn_budget = 100
+        max_cosine_distance = 0.3 #0.4 volt
+        nn_budget = 100 #100 volt
+        max_age = 90 # elfelejtési idő - tracker fenntartja a tracket 90 képkockán keresztül, miután az objektumot már nem észlelte - 20fps
+        n_init = 3 #hány megerősített detektálás szükséges egy track létrehozásához
         metric = NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
         self.tracker = DeepSortTracker(metric)
         self.tracks = []
@@ -43,6 +45,7 @@ class DeepSORT(BaseTracker):
     def update(self, detections): 
         self.tracker.predict()
         deepsort_detections = [Detection([(d.x - d.w), (d.y - d.h), d.w, d.h], d.conf, []) for d in detections]
+        #deepsort_detections = [Detection([(d.x - (d.w / 2)), (d.y - (d.h / 2)), (d.x + (d.w / 2)), (d.y + (d.h / 2))], d.conf, []) for d in detections]
         self.tracker.update(deepsort_detections)
         self.update_tracks()        
 
