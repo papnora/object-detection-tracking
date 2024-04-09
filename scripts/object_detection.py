27ef30390e605001ddccf631b102b5abacf2d90a
Pap Nóra <jamansardin@gmail.com>
2024-04-09 07:06:21 +0000

    Update datas folder

--- scripts/object_detection.py
+++ scripts/object_detection.py
@@ -1,16 +1,18 @@
-import torch
 import sys
 from pathlib import Path
-sys.path.append('/notebooks/ObjectDetectionTracking_PN/yolov7')
-import cv2 
-import yaml
+
+import cv2
 import numpy as np
+import pandas as pd
+import torch
+import yaml
+
+sys.path.append('/notebooks/ObjectDetectionTracking_PN/yolov7')
 from models.experimental import attempt_load  # YOLOv7 modell betöltése
-from torch import nn
 from utils.datasets import letterbox
 from utils.general import non_max_suppression, scale_coords, xyxy2xywh
 from utils.plots import plot_one_box
-import pandas as pd
+from torch import nn
 
 yaml_path = '/notebooks/ObjectDetectionTracking_PN/yolov7/data/coco.yaml'
 with open(yaml_path, 'r') as yaml_file:
@@ -31,97 +33,86 @@ def load_weights(weights: str, imgsz: int = 640, batch_size: int = 1, device: st
                 m._non_persistent_buffers_set = set()  # pytorch 1.6.0 compatibility
         return model[-1]
 
+def process_frame(frame, model, stride, names, frame_id, detections):  
+    img = letterbox(frame, 640, stride=stride)[0]  # Képkocka átméretezése és padding
+    img = img.transpose(2, 0, 1)  # BGR to RGB
+    img = np.ascontiguousarray(img)
+    img = torch.from_numpy(img).to('cuda')
+    img = img.half()
+    img /= 255.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
 
-model = attempt_load('/notebooks/ObjectDetectionTracking_PN/weights/yolov7-tiny.pt')  # Modellsúlyok elérési útvonala
-model.half()
-stride = int(model.stride.max())  # modell stride
-names = model.module.names if hasattr(model, 'module') else model.names
-model.eval()
+    # Detektálás
+    with torch.no_grad():
+        pred = model(img, augment=False)[0]
+        pred = non_max_suppression(pred, 0.4, 0.5, classes=None, agnostic=False)
 
-#video_path = '/notebooks/ObjectDetectionTracking_PN/datas/videos/hongkong_pedestrians.mp4'
-video_path = '/notebooks/ObjectDetectionTracking_PN/datas/videos/park_people.mp4'
-output_dir = Path('/notebooks/ObjectDetectionTracking_PN/datas/detections/')
-if not output_dir.exists():
-    output_dir.mkdir(parents=True)
-output_path = output_dir / Path(video_path).with_suffix(".csv").name
-detections = pd.DataFrame(columns=['frame_id','label', 'conf', 'x', 'y', 'w', 'h']) # x,y bounding box kp koodrinátái - w,h bounding box szélessége és magassága,  conf - confidencia, label  + cls az objektum osztályaz. (string , szám)
-if Path(output_path).exists():
-    fs = [f for f in output_dir.glob(f'*{output_path.stem}*')]
-    output_path = output_dir / (str(Path(video_path).stem)+f'{len(fs)}.csv')
-    print(output_path)
+    gn = torch.tensor(frame.shape)[[1, 0, 1, 0]]  # normalizált gain whwh
 
-cap = cv2.VideoCapture(video_path)
-ret, frame = cap.read()
-frame_id = 0  # Képkocka számláló
-width = frame.shape[1]
-height = frame.shape[0]
-print(frame.shape)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (width, height))
-try: 
+    # Detekciók rajzolása és rögzítése
+    for i, det in enumerate(pred):  # Detekciók az egyes képkockákhoz
+        if len(det):
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], frame.shape).round()
+            for *xyxy, conf, cls in det:
+                class_name = names[int(cls)]
+                label_with_index = f'{int(cls)}: {class_name} {conf:.2f}'
+                plot_one_box(xyxy, frame, label=label_with_index, color=(255, 0, 0), line_thickness=3)
+                xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4))).view(-1).cpu().tolist()  # normalizált xywh
+                cx, cy, w, h = xywh  # Itt cx,cy a középpont
+                # Átalakítjuk, hogy jobb alsó sarok legyen
+                x = cx + w / 2
+                y = cy + h / 2
+                row = {
+                    'frame_id': frame_id, 
+                    'label': int(cls.cpu()), 
+                    'conf': float(conf.cpu().item()), 
+                    'x': x, 
+                    'y': y, 
+                    'w': w, 
+                    'h': h
+                }
+              #  x,y,w,h = xywh
+               # row = {'frame_id': frame_id, 'label': int(cls.cpu()), 'conf': float(conf.cpu().item()), 'x': x, 'y': y, 'w': w, 'h': h}
+                print(row)
+                detections.loc[len(detections)] = row
+    return detections
+
+
+def process_video(video_path, output_path, model, names):    
+    cap = cv2.VideoCapture(video_path)
+    output_dir = Path(output_path).parent
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
+
+    detections = pd.DataFrame(columns=['frame_id', 'label', 'conf', 'x', 'y', 'w', 'h'])
+
+    frame_id = 0
+    stride = int(model.stride.max())  # modell stride
+    model.eval()
+
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
-            print('Videó vége')
             break
 
-        # Képkocka előkészítése
-        img = letterbox(frame, 640, stride=stride)[0]  # Képkocka átméretezése és padding
-        img = img.transpose(2, 0, 1)  # BGR to RGB
-        img = np.ascontiguousarray(img)
-        img = torch.from_numpy(img).to('cuda')
-        img = img.half()
-        
-        img /= 255.0
-        if img.ndimension() == 3:
-            img = img.unsqueeze(0)
+        detections = process_frame(frame, model, stride, names, frame_id, detections)
+        frame_id += 1
 
-        # Detektálás
-        with torch.no_grad():  #gradiensek számítása GPU memóriaszivárgást okozna
-            pred = model(img, augment=False)[0]
-            pred = non_max_suppression(pred, 0.4, 0.5, classes=None, agnostic=False)
-        
-        gn = torch.tensor(frame.shape)[[1, 0, 1, 0]]  # normalizált gain whwh
-        
-        # Detekciók rajzolása a képkockára
-        for i, det in enumerate(pred):  # Detekciók az egyes képkockákhoz
-            if len(det):
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], frame.shape).round()
-                for *xyxy, conf, cls in det:
-                    class_name = names[int(cls)]
-                    label_with_index = f'{int(cls)}: {class_name} {conf:.2f}'
-                    plot_one_box(xyxy, frame, label=label_with_index, color=(255, 0, 0), line_thickness=3)
-                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4))).view(-1).cpu().tolist()  # normalizált xywh
-                    cx, cy, w, h = xywh  # Itt cx,cy a középpont
-                    # Átalakítjuk, hogy jobb alsó sarok legyen
-                    x = cx + w / 2
-                    y = cy + h / 2
-                    row = {
-                        'frame_id': frame_id, 
-                        'label': int(cls.cpu()), 
-                        'conf': float(conf.cpu().item()), 
-                        'x': x, 
-                        'y': y, 
-                        'w': w, 
-                        'h': h
-                    }
-                  #  x,y,w,h = xywh
-                   # row = {'frame_id': frame_id, 'label': int(cls.cpu()), 'conf': float(conf.cpu().item()), 'x': x, 'y': y, 'w': w, 'h': h}
-                    print(row)
-                    detections.loc[len(detections)] = row
-                    #detections = detections.append(row, ignore_index=True)
-                    
-                    
-        out.write(frame)
-        frame_id += 1  # Képkocka számláló növelése
-        #if cv2.waitKey(1) == ord('q'):  # 'q' billentyűvel kilépés
-except KeyboardInterrupt:
-    print('Exiting...')
-finally:
-    detections.head(5)
-    detections.info()
     detections.to_csv(output_path, index=False)
+    cap.release()
 
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+model_path = '/notebooks/ObjectDetectionTracking_PN/weights/yolov7-tiny.pt'
+video_path = '/notebooks/ObjectDetectionTracking_PN/datas/videos/park_people.mp4'
+output_path = '/notebooks/ObjectDetectionTracking_PN/datas/detections/park_people_detections.csv'
+
+model = attempt_load(model_path)  # Modellsúlyok betöltése
+model.half()
+names = model.module.names if hasattr(model, 'module') else model.names
+model.eval()
+
+process_video(video_path, output_path, model, names)
+
+
+
+
