7a49cac0c69ead16d9d332981d954d01fabac3c6
Pap Nóra <jamansardin@gmail.com>
2024-04-21 17:25:14 +0000

    Calculating resources whilst trackings

--- scripts/starter_script.py
+++ scripts/starter_script.py
@@ -1,8 +1,10 @@
 import os
-import random
 import sys
+import time
+import random
 import subprocess
 from pathlib import Path
+from functools import wraps
 
 
 import cv2
@@ -10,6 +12,8 @@ import pandas as pd
 import torch
 import yaml
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+from memory_profiler import memory_usage
 from sklearn.model_selection import ParameterGrid
 
 sys.path.append('/notebooks/ObjectDetectionTracking_PN')
@@ -49,10 +53,47 @@ def save_tracking_to_csv(tracking_data, csv_path_tracking_output, video_path, tr
 def initialize_video_writer(video_path, width, height):
     return cv2.VideoWriter(str(video_path), cv2.VideoWriter_fourcc(*'DIVX'), 20.0, (width, height))
 
-def run_tracking(tracker_type, video_path, tracking_video_out, df, names, csv_path_tracking_output):
+def performance_monitor(func):
+    results = pd.DataFrame(columns=['Objektumkövető', 'Execution Time (s)', 'Memory Usage (MiB)'])
+    
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        tracker_type = args[0] 
+        #tracker_type = kwargs.get('tracker_type', 'Unknown')
+        print("TRACKER TYPE")
+        print(tracker_type)
+        start_time = time.perf_counter()
+        mem_usage_start = memory_usage()[0]
+        result = func(*args, **kwargs)  # Run the function
+        mem_usage_end = memory_usage()[0]
+        elapsed_time = time.perf_counter() - start_time
+        memory_used = mem_usage_end - mem_usage_start
+
+        print(f"Tracking completed in {elapsed_time:.4f} seconds.")
+        print(f"Memory used: {mem_usage_end - mem_usage_start} MiB")
+        
+        # Store results
+        results.loc[len(results)] = [tracker_type,  elapsed_time, memory_used]
+        
+        if 'save_performance_data' in kwargs and kwargs['save_performance_data']:
+            save_performance_to_excel(results, kwargs.get('metrics_path', './'))
+
+        return result
+    
+    return wrapper
+
+def save_performance_to_excel(df, path):
+   # Ensure the directory exists
+    Path(path).mkdir(parents=True, exist_ok=True)
+    # Save the DataFrame to an Excel file
+    excel_path = os.path.join(path, "resource_performance_data.xlsx")
+    df.to_excel(excel_path, index=False)
+    print(f"Performance data saved to Excel at: {excel_path}")
+
+@performance_monitor
+def run_tracking(tracker_type, video_path, tracking_video_out, df, names, csv_path_tracking_output, **kwargs):
     colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for j in range(10)]
     tracking_data = pd.DataFrame(columns=['frame_id', 'id', 'bbox_x', 'bbox_y', 'bbox_width', 'bbox_height', 'presence'])
-
     
     cap = cv2.VideoCapture(video_path)
     ret, frame = cap.read()
@@ -73,7 +114,9 @@ def run_tracking(tracker_type, video_path, tracking_video_out, df, names, csv_pa
     print(f"\nRunning {tracker_type} tracking...\n")
 
     out = initialize_video_writer(tracking_video_out, width, height)
-
+    kwargs['tracker_type'] = tracker_type
+    
+    
     try:
         for _ in tqdm(range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))):
             ret, frame = cap.read()
@@ -123,25 +166,19 @@ def main():
     csv_path_tracking_output = '/notebooks/ObjectDetectionTracking_PN/datas/trackings/'
 
     video_path = '/notebooks/ObjectDetectionTracking_PN/datas/videos/park_people.mp4'
+    metrics_path = '/notebooks/ObjectDetectionTracking_PN/datas/metrics/'
 
-    #param_grid_bytetrack = ParameterGrid(
-     #   {
-      #      "param_1": [1,2,3],
-       #     "param_2": [4,5,6],
-       # }
-    #)
-        # Run tracking with ByteTrack
+    # Run tracking with ByteTrack
     tracking_video_out_byte = Path('/notebooks/ObjectDetectionTracking_PN/datas/videos/bytetrack_out.avi')
-    run_tracking('bytetrack', video_path, tracking_video_out_byte, df, names, csv_path_tracking_output)
-        #TODO: Run eval script then save output into table ...
+    run_tracking('bytetrack', video_path, tracking_video_out_byte, df, names, csv_path_tracking_output, save_performance_data=True, metrics_path=metrics_path)
         
     # Run tracking with DeepSORT
     tracking_video_out_deep = Path('/notebooks/ObjectDetectionTracking_PN/datas/videos/deepsort_out.avi')
-    run_tracking('deepsort', video_path, tracking_video_out_deep, df, names, csv_path_tracking_output)
+    run_tracking('deepsort', video_path, tracking_video_out_deep, df, names, csv_path_tracking_output, save_performance_data=True, metrics_path=metrics_path)
     
     # Run tracking with BotSORT
     tracking_video_out_bot = Path('/notebooks/ObjectDetectionTracking_PN/datas/videos/botsort_out.avi')
-    run_tracking('botsort', video_path, tracking_video_out_bot, df, names, csv_path_tracking_output)
+    run_tracking('botsort', video_path, tracking_video_out_bot, df, names, csv_path_tracking_output, save_performance_data=True, metrics_path=metrics_path)
 
 if __name__ == "__main__":
     main()
