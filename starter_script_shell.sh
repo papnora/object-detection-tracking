d28cd1059f0c658658ffbeee24183c10446b2e0a
Pap Nóra <jamansardin@gmail.com>
2024-04-21 13:15:59 +0000

    Update

--- starter_script_shell.sh
+++ starter_script_shell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=/notebooks/ObjectDetectionTracking_PN/ByteTrack/
-pip install loguru lap thop cython_bbox yacs faiss-gpu motmetrics
+pip install loguru lap thop cython_bbox yacs faiss-gpu motmetrics memory_profiler
 python /notebooks/ObjectDetectionTracking_PN/scripts/starter_script.py
